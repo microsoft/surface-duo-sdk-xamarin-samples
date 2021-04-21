b725bee36981658a29a507706dead1aa90290939
Craig Dunn <craig.dunn@microsoft.com>
2021-04-21 14:03:17 -0700

    [CompanionPane] AndroidX.Window alpha05

--- CompanionPane/MainActivity.cs
+++ CompanionPane/MainActivity.cs
@@ -1,13 +1,15 @@
 ﻿
 using Android.Content.Res;
 using Android.OS;
-using Android.Runtime;
+using Android.Util;
 using Android.Views;
-using Android.Widget;
 using AndroidX.AppCompat.App;
+using AndroidX.Core.Util;
 using AndroidX.Fragment.App;
+using AndroidX.Window;
 using CompanionPane.Fragments;
-using Microsoft.Device.Display;
+using Java.Lang;
+using Java.Util.Concurrent;
 using System.Linq;
 
 namespace CompanionPane
@@ -18,14 +20,17 @@ namespace CompanionPane
 		RoundIcon = "@mipmap/ic_launcher_round",
 		Theme = "@style/AppTheme",
 		MainLauncher = true,
-		ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.ScreenLayout | Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.SmallestScreenSize)]
-	public class MainActivity : AppCompatActivity, BaseFragment.IOnItemSelectedListener
+		ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.ScreenLayout | Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.SmallestScreenSize | Android.Content.PM.ConfigChanges.UiMode)]
+	public class MainActivity : AppCompatActivity, BaseFragment.IOnItemSelectedListener, IConsumer
 	{
+		const string TAG = "JWM"; // Jetpack Window Manager
+		WindowManager wm;
+		int hingeOrientation = FoldingFeature.OrientationVertical;
+		bool isDuo, isDualMode;
+
 		SinglePortrait singlePortrait;
 		DualPortrait dualPortrait;
 		DualLandscape dualLandscape;
-		ScreenHelper screenHelper;
-		bool isDuo;
 		int currentPosition;
 
 		protected override void OnCreate(Bundle savedInstanceState)
@@ -33,10 +38,6 @@ namespace CompanionPane
 			base.OnCreate(savedInstanceState);
 
 			SetContentView(Resource.Layout.activity_main);
-
-			screenHelper = new ScreenHelper();
-
-			isDuo = screenHelper.Initialize(this);
 
 			var slides = Slide.Slides.ToList();
 
@@ -47,7 +48,66 @@ namespace CompanionPane
 			dualPortrait.RegisterOnItemSelectedListener(this);
 			dualLandscape = DualLandscape.NewInstance(slides);
 			dualLandscape.RegisterOnItemSelectedListener(this);
+
+			wm = new WindowManager(this);
+
 			SetupLayout();
+		}
+
+
+		IExecutor runOnUiThreadExecutor()
+		{
+			return new MyExecutor();
+		}
+		class MyExecutor : Java.Lang.Object, IExecutor
+		{
+			Handler handler = new Handler(Looper.MainLooper);
+			public void Execute(IRunnable r)
+			{
+				handler.Post(r);
+			}
+		}
+
+		public void Accept(Java.Lang.Object newLayoutInfo)  // Object will be WindowLayoutInfo
+		{
+			Log.Info(TAG, "===LayoutStateChangeCallback.Accept");
+			var wli = newLayoutInfo as WindowLayoutInfo;
+			if (wli.DisplayFeatures.Count == 0)
+			{ // no hinge found
+				isDualMode = false;
+			}
+			else
+			{
+				foreach (var df in wli.DisplayFeatures)
+				{
+					Log.Info(TAG, "Bounds:" + df.Bounds);
+					var ff = df as FoldingFeature;
+					if (!(ff is null))
+					{   // a hinge exists
+						Log.Info(TAG, "Orientation: " + ff.Orientation);
+						isDualMode = true;
+						hingeOrientation = ff.Orientation;
+						isDuo = true; //HACK: set first time we see the hinge, never un-set
+					}
+					else
+					{ // no hinge found
+						isDualMode = false;
+					}
+				}
+			}
+			SetupLayout();
+		}
+
+		public override void OnAttachedToWindow()
+		{
+			base.OnAttachedToWindow();
+			wm.RegisterLayoutChangeCallback(runOnUiThreadExecutor(), this);
+		}
+
+		public override void OnDetachedFromWindow()
+		{
+			base.OnDetachedFromWindow();
+			wm.UnregisterLayoutChangeCallback(this);
 		}
 
 		void ShowFragment(Fragment fragment)
@@ -72,16 +132,12 @@ namespace CompanionPane
 		public override void OnConfigurationChanged(Configuration newConfig)
 		{
 			base.OnConfigurationChanged(newConfig);
-
-			if(ScreenHelper.IsDualScreenDevice(this))
-				screenHelper.Update();
-
 			SetupLayout();
 		}
 
-		void UseDualMode(SurfaceOrientation rotation)
+		void UseDualMode(int hingeOrientation)
 		{
-			if (rotation == SurfaceOrientation.Rotation90 || rotation == SurfaceOrientation.Rotation270)
+			if (hingeOrientation == FoldingFeature.OrientationHorizontal)
 			{
 				dualLandscape.setCurrentPosition(currentPosition);
 				ShowFragment(dualLandscape);
@@ -107,12 +163,10 @@ namespace CompanionPane
 
 		void SetupLayout()
 		{
-			var rotation = ScreenHelper.GetRotation(this);
-
 			if (isDuo)
 			{
-				if (screenHelper.IsDualMode)
-					UseDualMode(rotation);
+				if (isDualMode)
+					UseDualMode(hingeOrientation);
 				else
 					UseSingleMode();
 			}
