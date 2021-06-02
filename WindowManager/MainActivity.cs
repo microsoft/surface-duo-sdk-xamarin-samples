3c0b28f38a476193b42d014c56497f6e28afb78b
Craig Dunn <craig.dunn@microsoft.com>
2021-06-02 12:59:05 -0700

    formatting fixes

--- WindowManager/MainActivity.cs
+++ WindowManager/MainActivity.cs
@@ -18,8 +18,8 @@ using Java.Util.Concurrent;
  */
 namespace WindowManagerDemo
 {
-    [Activity(Label = "@string/app_name", 
-    Theme = "@style/AppTheme", 
+    [Activity(Label = "@string/app_name",
+    Theme = "@style/AppTheme",
     MainLauncher = true)]//, // HACK: for some reason the Window Manager doesn't work when configuration changes are being handled 
     //ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.ScreenLayout | Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.SmallestScreenSize)]
     public class MainActivity : AppCompatActivity, IConsumer
@@ -27,8 +27,8 @@ namespace WindowManagerDemo
         const string TAG = "JWM"; // Jetpack Window Manager
         WindowManager wm;
 
-		ConstraintLayout constraintLayout;
-		TextView windowMetrics, layoutChange, configurationChanged;
+        ConstraintLayout constraintLayout;
+        TextView windowMetrics, layoutChange, configurationChanged;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -38,30 +38,30 @@ namespace WindowManagerDemo
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-			constraintLayout = FindViewById<ConstraintLayout>(Resource.Id.constraint_layout);
-			windowMetrics = FindViewById<TextView>(Resource.Id.window_metrics);
-			layoutChange = FindViewById<TextView>(Resource.Id.layout_change);
-			configurationChanged = FindViewById<TextView>(Resource.Id.configuration_changed);
-		}
+            constraintLayout = FindViewById<ConstraintLayout>(Resource.Id.constraint_layout);
+            windowMetrics = FindViewById<TextView>(Resource.Id.window_metrics);
+            layoutChange = FindViewById<TextView>(Resource.Id.layout_change);
+            configurationChanged = FindViewById<TextView>(Resource.Id.configuration_changed);
+        }
 
-		void printLayoutStateChange(WindowLayoutInfo newLayoutInfo)
-		{
-			Log.Info(TAG, wm.CurrentWindowMetrics.Bounds.ToString());
-			Log.Info(TAG, wm.MaximumWindowMetrics.Bounds.ToString());
-			windowMetrics.Text = $"CurrentWindowMetrics: {wm.CurrentWindowMetrics.Bounds}\n" +
-				$"MaximumWindowMetrics: {wm.MaximumWindowMetrics.Bounds}";
-			
-			layoutChange.Text = newLayoutInfo.ToString();
+        void printLayoutStateChange(WindowLayoutInfo newLayoutInfo)
+        {
+            Log.Info(TAG, wm.CurrentWindowMetrics.Bounds.ToString());
+            Log.Info(TAG, wm.MaximumWindowMetrics.Bounds.ToString());
+            windowMetrics.Text = $"CurrentWindowMetrics: {wm.CurrentWindowMetrics.Bounds}\n" +
+                $"MaximumWindowMetrics: {wm.MaximumWindowMetrics.Bounds}";
 
-			configurationChanged.Text = "One logic/physical display - unspanned";
+            layoutChange.Text = newLayoutInfo.ToString();
+
+            configurationChanged.Text = "One logic/physical display - unspanned";
 
             foreach (var displayFeature in newLayoutInfo.DisplayFeatures)
             {
                 if (displayFeature is FoldingFeature foldingFeature)
                 {
-					alignViewToDeviceFeatureBoundaries(newLayoutInfo);
+                    alignViewToDeviceFeatureBoundaries(newLayoutInfo);
 
-					if (foldingFeature.OcclusionMode == FoldingFeature.OcclusionNone)
+                    if (foldingFeature.OcclusionMode == FoldingFeature.OcclusionNone)
                     {
                         configurationChanged.Text = "App is spanned across a fold";
                     }
@@ -72,112 +72,115 @@ namespace WindowManagerDemo
                     configurationChanged.Text += "\nIsSeparating: " + foldingFeature.IsSeparating
                             + "\nOrientation: " + foldingFeature.Orientation  // FoldingFeature.OrientationVertical or Horizontal
                             + "\nState: " + foldingFeature.State; // FoldingFeature.StateFlat or StateHalfOpened
-				}
+                }
                 else
                 {
                     Log.Info(TAG, "DisplayFeature is not a fold or hinge");
                 }
             }
-		}
+        }
 
-		void alignViewToDeviceFeatureBoundaries(WindowLayoutInfo newLayoutInfo)
-		{
-			var set = new ConstraintSet();
-			set.Clone(constraintLayout); // existing constraints baseline
-			var foldFeature = newLayoutInfo.DisplayFeatures[0] as FoldingFeature;
-			//We get the display feature bounds.
-			var rect = foldFeature.Bounds;
-			//Set the red hinge indicator's width and height using the Bounds
-			set.ConstrainHeight(Resource.Id.device_feature, rect.Bottom - rect.Top);
-			set.ConstrainWidth(Resource.Id.device_feature, rect.Right - rect.Left);
-			
-			set.Connect(
-				Resource.Id.device_feature, ConstraintSet.Start,
-				ConstraintSet.ParentId, ConstraintSet.Start, 0
-			);
+        void alignViewToDeviceFeatureBoundaries(WindowLayoutInfo newLayoutInfo)
+        {
+            var set = new ConstraintSet();
+            set.Clone(constraintLayout); // existing constraints baseline
+            var foldFeature = newLayoutInfo.DisplayFeatures[0] as FoldingFeature;
+            //We get the display feature bounds.
+            var rect = foldFeature.Bounds;
+            //Set the red hinge indicator's width and height using the Bounds
+            set.ConstrainHeight(Resource.Id.device_feature, rect.Bottom - rect.Top);
+            set.ConstrainWidth(Resource.Id.device_feature, rect.Right - rect.Left);
 
-			set.Connect(
-				Resource.Id.device_feature, ConstraintSet.Top,
-				ConstraintSet.ParentId, ConstraintSet.Top, 0
-			);
+            set.Connect(
+                Resource.Id.device_feature, ConstraintSet.Start,
+                ConstraintSet.ParentId, ConstraintSet.Start, 0
+            );
 
-			if (foldFeature.Orientation == FoldingFeature.OrientationVertical)
-			{
-				// Device feature is placed vertically
-				set.SetMargin(Resource.Id.device_feature, ConstraintSet.Start, rect.Left);
+            set.Connect(
+                Resource.Id.device_feature, ConstraintSet.Top,
+                ConstraintSet.ParentId, ConstraintSet.Top, 0
+            );
+
+            if (foldFeature.Orientation == FoldingFeature.OrientationVertical)
+            {
+                // Device feature is placed vertically
+                set.SetMargin(Resource.Id.device_feature, ConstraintSet.Start, rect.Left);
                 set.Connect(
                     Resource.Id.layout_change, ConstraintSet.End,
                     Resource.Id.device_feature, ConstraintSet.Start, 0
                 );
             }
-			else
-			{
-				//Device feature is placed horizontally
-				var statusBarHeight = calculateStatusBarHeight();
-				var toolBarHeight = calculateToolbarHeight();
+            else
+            {
+                //Device feature is placed horizontally
+                var statusBarHeight = calculateStatusBarHeight();
+                var toolBarHeight = calculateToolbarHeight();
 
-				set.SetMargin(
-					Resource.Id.device_feature, ConstraintSet.Top,
-					rect.Top - statusBarHeight - toolBarHeight
-				);
+                set.SetMargin(
+                    Resource.Id.device_feature, ConstraintSet.Top,
+                    rect.Top - statusBarHeight - toolBarHeight
+                );
                 set.Connect(
                     Resource.Id.layout_change, ConstraintSet.Top,
                     Resource.Id.device_feature, ConstraintSet.Bottom, 0
                 );
             }
-			
-			//Set the view to visible and apply constraints
-			set.SetVisibility(Resource.Id.device_feature, (int)SystemUiFlags.Visible); // public static final int VISIBLE = 0x00000000;
-			set.ApplyTo(constraintLayout);
-		}
 
-		int calculateToolbarHeight()
-		{
-			var typedValue = new TypedValue();
-			if (Theme.ResolveAttribute(Android.Resource.Attribute.ActionBarSize, typedValue, true)) {
-				return TypedValue.ComplexToDimensionPixelSize(typedValue.Data, Resources.DisplayMetrics);
-			} else {
-				return 0;
-			}
-	    }
+            //Set the view to visible and apply constraints
+            set.SetVisibility(Resource.Id.device_feature, (int)SystemUiFlags.Visible); // public static final int VISIBLE = 0x00000000;
+            set.ApplyTo(constraintLayout);
+        }
 
-		int calculateStatusBarHeight()
-		{
-			var rect = new Rect();
-			Window.DecorView.GetWindowVisibleDisplayFrame(rect);
-			return rect.Top;
-		}
+        int calculateToolbarHeight()
+        {
+            var typedValue = new TypedValue();
+            if (Theme.ResolveAttribute(Android.Resource.Attribute.ActionBarSize, typedValue, true))
+            {
+                return TypedValue.ComplexToDimensionPixelSize(typedValue.Data, Resources.DisplayMetrics);
+            }
+            else
+            {
+                return 0;
+            }
+        }
 
-		IExecutor runOnUiThreadExecutor()
-		{
-			return new MyExecutor();
-		}
-		class MyExecutor : Java.Lang.Object, IExecutor
-		{
-			Handler handler = new Handler(Looper.MainLooper);
-			public void Execute(IRunnable r)
-			{
-				handler.Post(r);
-			}
-		}
+        int calculateStatusBarHeight()
+        {
+            var rect = new Rect();
+            Window.DecorView.GetWindowVisibleDisplayFrame(rect);
+            return rect.Top;
+        }
 
-		public void Accept(Java.Lang.Object newLayoutInfo)  // Object will be WindowLayoutInfo
-		{
-			Log.Info(TAG, "===LayoutStateChangeCallback.Accept");
-			Log.Info(TAG, newLayoutInfo.ToString());
-			printLayoutStateChange(newLayoutInfo as WindowLayoutInfo);
-		}
+        IExecutor runOnUiThreadExecutor()
+        {
+            return new MyExecutor();
+        }
+        class MyExecutor : Java.Lang.Object, IExecutor
+        {
+            Handler handler = new Handler(Looper.MainLooper);
+            public void Execute(IRunnable r)
+            {
+                handler.Post(r);
+            }
+        }
 
-		public override void OnAttachedToWindow()
-		{
-			base.OnAttachedToWindow();
-			wm.RegisterLayoutChangeCallback(runOnUiThreadExecutor(), this);
-		}
+        public void Accept(Java.Lang.Object newLayoutInfo)  // Object will be WindowLayoutInfo
+        {
+            Log.Info(TAG, "===LayoutStateChangeCallback.Accept");
+            Log.Info(TAG, newLayoutInfo.ToString());
+            printLayoutStateChange(newLayoutInfo as WindowLayoutInfo);
+        }
 
-		public override void OnDetachedFromWindow()
-		{
-			base.OnDetachedFromWindow();
-			wm.UnregisterLayoutChangeCallback(this);
-		}
-	}
+        public override void OnAttachedToWindow()
+        {
+            base.OnAttachedToWindow();
+            wm.RegisterLayoutChangeCallback(runOnUiThreadExecutor(), this);
+        }
+
+        public override void OnDetachedFromWindow()
+        {
+            base.OnDetachedFromWindow();
+            wm.UnregisterLayoutChangeCallback(this);
+        }
+    }
 }
